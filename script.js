85a1be35197b1b66de0360bad33e2050e2327c99
shivansh <shivneeraj2004@gmail.com>
2024-04-14 21:18:14 +0530

    almost done

--- script.js
+++ script.js
@@ -13,13 +13,17 @@ const hideItems = () => {
     links.forEach(el => {
 
 
+
         if (el.children[1].className.split(" ").includes("hide")) {
             el.children[1].classList.add("unhide")
             el.children[1].classList.remove("hide")
+            // el.children[1].classList.remove("hidden")
+            // el.classList.remove("tt")
         }
         else {
             el.children[1].classList.add("hide")
             el.children[1].classList.remove("unhide")
+            // el.classList.toggle("tt")
 
         }
 
@@ -32,7 +36,8 @@ const hideItems = () => {
         collapse.children[1].classList.add("unhide")
         collapse.children[1].classList.remove("hide")
         console.log(collapse2.children[0])
-        collapse2.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#ffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-align-justify"><line x1="21" y1="10" x2="3" y2="10"></line><line x1="21" y1="6" x2="3" y2="6"></line><line x1="21" y1="14" x2="3" y2="14"></line><line x1="21" y1="18" x2="3" y2="18"></line></svg>'
+        // collapse2.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#ffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-align-justify"><line x1="21" y1="10" x2="3" y2="10"></line><line x1="21" y1="6" x2="3" y2="6"></line><line x1="21" y1="14" x2="3" y2="14"></line><line x1="21" y1="18" x2="3" y2="18"></line></svg>'
+        collapse2.innerHTML = '<svg svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#ffff" stroke - width="2" stroke - linecap="round" stroke - linejoin="round" class="feather feather-align-right" ><line x1="21" y1="10" x2="7" y2="10"></line><line x1="21" y1="6" x2="3" y2="6"></line><line x1="21" y1="14" x2="3" y2="14"></line><line x1="21" y1="18" x2="7" y2="18"></line></svg >'
 
 
 
@@ -43,7 +48,8 @@ const hideItems = () => {
     else {
         collapse.children[1].classList.add("hide")
         collapse.children[1].classList.remove("unhide")
-        collapse2.innerHTML = '<svg svg xmlns = "http://www.w3.org/2000/svg" width = "18" height = "18" viewBox = "0 0 24 24" fill = "none" stroke = "#ffff" stroke - width="2" stroke - linecap="round" stroke - linejoin="round" class="feather feather-align-right" ><line x1="21" y1="10" x2="7" y2="10"></line><line x1="21" y1="6" x2="3" y2="6"></line><line x1="21" y1="14" x2="3" y2="14"></line><line x1="21" y1="18" x2="7" y2="18"></line></svg > '
+
+        collapse2.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#ffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-align-justify"><line x1="21" y1="10" x2="3" y2="10"></line><line x1="21" y1="6" x2="3" y2="6"></line><line x1="21" y1="14" x2="3" y2="14"></line><line x1="21" y1="18" x2="3" y2="18"></line></svg>'
 
 
 
